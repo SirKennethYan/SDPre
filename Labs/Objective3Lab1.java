e69800078efef7dc674572b8ab0830ee3395a84b
kennyan11b <kennyan11b@gmail.com>
2022-12-14 15:40:39 -0800

    Average

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,13 @@
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+
+    int num1 = 15;
+    int num2 = 6;
+    int num3 = 2;
+
+    int average = (num1 + num2 + num3) / 3;
+
+    System.out.println(average);
+
+  }
+}
