e7fb8900678f14e1718b3658c656c47b349f6d64
kennyan11b <kennyan11b@gmail.com>
2022-12-13 19:55:18 -0800

    Year, Month, Weekend

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,13 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+
+    int yearBorn = 1985
+    String monthBorn = May
+    String weekend = False
+
+    System.out.println(yearBorn);
+    System.out.println(monthBorn);
+    System.out.println(weekend);
+
+  }
+}
