75f7f786e17c3a7e72f1f1cfbaead9acff87b918
kennyan11b <kennyan11b@gmail.com>
2022-12-13 20:03:18 -0800

    Year, Month, Weekend

--- Labs/Objective2Lab3.java
+++ Labs/Objective2Lab3.java
@@ -1,9 +1,9 @@
 public class Objective2Lab3 {
   public static void main(String[] args) {
 
-    int yearBorn = 1985
-    String monthBorn = May
-    String weekend = False
+    int yearBorn = 1985;
+    String monthBorn = "May";
+    String weekend = "False";
 
     System.out.println(yearBorn);
     System.out.println(monthBorn);
