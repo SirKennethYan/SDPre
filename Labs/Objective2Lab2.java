992b515d07038ccc937cd038561e30b6e7fed648
kennyan11b <kennyan11b@gmail.com>
2022-12-13 19:22:30 -0800

    Name, Age, State

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,11 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Kenny Yan";
+    int age = 37;
+    String state = "California";
+
+    System.out.println("Name: " + name);
+    System.out.println("Age: " + age);
+    System.out.println("State: " + state);
+  }
+}
