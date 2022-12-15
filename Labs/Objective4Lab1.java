854a6b17c1df0b99a02341e3673c42d4187929fd
kennyan11b <kennyan11b@gmail.com>
2022-12-15 14:49:09 -0800

    main

---
+++ Labs/Objective4Lab1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Objective4Lab1 {
+  public static void main(String[] args) {
+
+    Scanner keyboard = new Scanner(System.in);
+
+    String fname = keyboard.nextLine();
+    String lname = keyboard.nextLine();
+    String favoriteAnimal = keyboard.nextLine();
+    String favoriteFood = keyboard.nextLine();
+    String favoriteSong = keyboard.nextLine();
+
+    System.out.println("My name is " + fname + lname);
+    System.out.println("My favorite animal is " + favoriteAnimal);
+    System.out.println("My favorite food is " + favoriteFood);
+    System.out.println("My favorite song is " + favoriteSong);
+
+  }
+}
