863d8d3a64e61bde07ea5cac326769a75f2c9baa
kennyan11b <kennyan11b@gmail.com>
2022-12-14 17:15:58 -0800

    Hypotenuse

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,13 @@
+public class Objective3Lab2 {
+  public static void main(String[] args) {
+
+    double side1, side2, hypotenuse;
+
+    side1 = 8;
+    side2 = 10;
+
+    hypotenuse = Math.sqrt(Math.pow(side1, 2) + Math.pow(side2, 2));
+
+    System.out.println(hypotenuse);
+  }
+}
