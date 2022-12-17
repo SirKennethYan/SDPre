633162a33c8ce35b44fcbc9f90470b6c070efe92
kennyan11b <kennyan11b@gmail.com>
2022-12-16 23:53:10 -0800

    Odd or Even

---
+++ Labs/Objective5Lab4.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Objective5Lab4 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		//int userNum;
+
+		System.out.println("Please enter a number: ");
+		int userNum = scanner.nextInt();
+
+  	if(userNum % 2 == 0) {
+    //would determine if a number is even
+  		System.out.println("The number is even");
+  	}
+  	else {
+  		System.out.println("The number is odd");
+  	}
+  }
+}
